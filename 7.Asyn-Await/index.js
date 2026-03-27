80e2fc6ac6d5f556099378be166596ced5c2b84b
Ansh Pratap <anshpratap866@gmail.com>
2026-03-27 23:19:50 +0530

    Async-Await

---
+++ 7.Asyn-Await/index.js
@@ -0,0 +1,12 @@
+async function main() {
+    try{
+        const result = await divide(10,2);
+        console.log(result);
+    }catch(err){
+        console.log(err);
+
+    }
+    
+    
+}
+main();
