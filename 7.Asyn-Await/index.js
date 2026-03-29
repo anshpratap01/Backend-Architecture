ddd6898a54008e8861d52a1ece975b3648c4818e
Ansh Pratap <anshpratap866@gmail.com>
2026-03-29 23:44:57 +0530

    event emiiter  .on .emit

--- 7.Asyn-Await/index.js
+++ 7.Asyn-Await/index.js
@@ -1,3 +1,15 @@
+function divide(num1, num2) {
+  return new Promise(function(resolve, reject) {
+
+    if (num2 === 0) {
+      reject('Error: Cannot divide by zero!');
+    } else {
+      resolve(num1 / num2);
+    }
+
+  });
+}
+
 async function main() {
     try{
         const result = await divide(10,2);
